fc97c749cc33fe3de3c1d6b0e74014fe121b4584
Aidan <am029620@ohio.edu>
2022-01-18 20:18:12 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Aidan McCargish>
+ *        Date: <January 18, 2022>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
